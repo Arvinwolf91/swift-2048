72461ba2497762ef9108ee7d6a957502869f84dc
Dan Coman <dancoman@Dans-MacBook-Air.local>
2018-02-05 14:40:47 +0800

    added React Navigation - StackNavigator

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import {AppRegistry, StyleSheet, Text, View} from 'react-native';
+import {AppRegistry, StyleSheet, Text, View, Button} from 'react-native';
+import { StackNavigator } from 'react-navigation';
 
 class RNHighScores extends React.Component {
   render() {
-    var contents = this.props['scores'].map((score) => (
+    var contents = this.props.screenProps.scores.map((score) => (
       <Text key={score.name}>
         {score.name}:{score.value}
         {'\n'}
@@ -13,8 +14,42 @@ class RNHighScores extends React.Component {
       <View style={styles.container}>
         <Text style={styles.highScoresTitle}>2048 High Scores!</Text>
         <Text style={styles.scores}>{contents}</Text>
+        <Button
+          title="Go to Details"
+          onPress={() => this.props.navigation.navigate('Details')}
+        />
       </View>
     );
+  }
+}
+
+class DetailsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+      </View>
+    );
+  }
+}
+
+const RootStack = StackNavigator(
+  {
+    Home: {
+      screen: RNHighScores,
+    },
+    Details: {
+      screen: DetailsScreen,
+    },
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
+
+export default class App extends React.Component {
+  render() {
+    return <RootStack screenProps={this.props}/>;
   }
 }
 
@@ -38,4 +73,4 @@ const styles = StyleSheet.create({
 });
 
 // Module name
-AppRegistry.registerComponent('RNHighScores', () => RNHighScores);
+AppRegistry.registerComponent('RNHighScores', () => App);
